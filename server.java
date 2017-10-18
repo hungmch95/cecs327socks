eb5df271647bad2f6f9ba45045a8182629cef52f
Daeyang <nguyendavid095@gmail.com>
2017-10-17 19:51:14 -0700

    Create server.java

---
+++ server.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.*;
+import java.net.*;
+/**
+ *
+ * @author David Nguyen
+ */
+public class server {
+    
+    public static void main(String[] args) throws Exception{
+            server SERVER = new server();
+            SERVER.run();
+    }
+    
+    public void run() throws Exception
+    {
+        ServerSocket ServerSocket = new ServerSocket(444);
+        Socket SOCK = ServerSocket.accept();
+        InputStreamReader IR = new InputStreamReader(SOCK.getInputStream());
+        BufferedReader BR = new BufferedReader(IR);
+        
+        String message = BR.readLine();
+        System.out.println(message);
+        
+        if(message != null){
+            PrintStream ps = new PrintStream(SOCK.getOutputStream());
+                    ps.println("Message received");
+        }
+    }
+}
